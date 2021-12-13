3e2f57f5a3b452b01383efc1e113743789f65708
havo2001 <84851139+havo2001@users.noreply.github.com>
2021-12-13 10:28:52 +0300

    Delete MyGame directory

--- MyGame/source/menu.cpp
+++
@@ -1,7 +0,0 @@
-#include "../header/menu.h"
-const int widthSize = 900;
-const int heightSize = 600;
-menu::menu(QGraphicsItem *parent) : QGraphicsPixmapItem(parent) {
-  setPixmap(QPixmap(":/image/background1.jpg").scaled(widthSize, heightSize));
-  setPos(0, 0);
-}
