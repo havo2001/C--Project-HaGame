6ff9a37dacc0d7e048e3e00e3b89541e860fff5b
havo2001 <84851139+havo2001@users.noreply.github.com>
2021-12-13 21:17:31 +0300

    Add files via upload

---
+++ test/tst_test1.cpp
@@ -0,0 +1,37 @@
+#include <QtTest>
+#include <hagame.h>
+
+// add necessary includes here
+
+class test1 : public QObject
+{
+    Q_OBJECT
+
+public:
+    test1();
+    ~test1();
+
+private slots:
+    void test_case1();
+
+};
+
+test1::test1()
+{
+   QCOMPARE(2, GetCoordX(560));
+   QCOMPARE(1, GetCoordY(180));
+}
+
+test1::~test1()
+{
+    //default
+}
+
+void test1::test_case1()
+{
+
+}
+
+QTEST_APPLESS_MAIN(test1)
+
+#include "tst_test1.moc"
