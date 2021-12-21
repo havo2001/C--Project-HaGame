a59f840f81dae6c0908d481af8ba8c85aca11146
havo2001 <84851139+havo2001@users.noreply.github.com>
2021-12-21 13:52:56 +0300

    Update score.cpp

--- src/score.cpp
+++ src/score.cpp
@@ -2,21 +2,25 @@
 #include <QFont>
 #include <QFontDatabase>
 
+const QString fontPath = ":/font/AmongYou-BWdWw.ttf";
 const int initScore = 0;
 const int fontSize = 15;
 std::pair<int, int> initPos(730 ,70);
+
 score::score(QGraphicsItem *parent) : QGraphicsTextItem(parent) {
   total = initScore;
-  QFontDatabase::addApplicationFont(":/font/AmongYou-BWdWw.ttf");
+  QFontDatabase::addApplicationFont(fontPath);
   QFont font("a Among You", fontSize);
   setPos(initPos.first, initPos.second);
   setFont(font);
   setDefaultTextColor(QColor(125, 211, 217, 255));
 
 }
+
 int score::getScore() {
   return total;
 }
+
 void score::setScore(int value) {
   total = value;
   setPlainText("Score: " + QString::number(total));
