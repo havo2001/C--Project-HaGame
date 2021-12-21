015fadeb9af723d3c03c51b98c06e1edb7b9ddc8
havo2001 <84851139+havo2001@users.noreply.github.com>
2021-12-21 14:02:27 +0300

    Update menu.cpp

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,10 +1,12 @@
 #include "include/menu.h"
 
+
 const int widthSize = 900;
 const int heightSize = 600;
 const std::pair <int, int> initPos(0, 0);
+const QString backgroundImage = ":/image/background1.jpg";
 
 menu::menu(QGraphicsItem *parent) : QGraphicsPixmapItem(parent) {
-  setPixmap(QPixmap(":/image/background1.jpg").scaled(widthSize, heightSize));
+  setPixmap(QPixmap(backgroundImage).scaled(widthSize, heightSize));
   setPos(initPos.first, initPos.second);
 }
